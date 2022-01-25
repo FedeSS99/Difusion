dc6d54e5b015c6c67b897c90a1b63bda9c61acfc
FedericoSS <federico.salinassmng@uanl.edu.mx>
2022-01-25 17:12:05 -0600

    Modified .py and .pyx files

--- Difusion_Limitada.py
+++ Difusion_Limitada.py
@@ -1,34 +1,23 @@
-import numpy as np
-from math import hypot
+from numpy import zeros, float64
 from pyqtgraph.Qt import QtCore
-from pyqtgraph.exporters import ImageExporter
 import pyqtgraph as pg
-from time import perf_counter
-from os import system
 
 from RutinasDifusion import DifusionLim
 
-
 #Dimensiones de la "caja"
-N = 200
+N = 512
 #Numero de celdas caminantes
-Nceldas =  3000
-radioMax = 4
-
-PosiblesMovimientos = [(1,1),(1,-1),(-1,-1),(-1,1)]
-
-kernel = np.array([[1,1,1],[1,0,1],[1,1,1]], dtype=np.float64)
-dist = hypot(*PosiblesMovimientos[0])
+Nceldas =  20000
 
 #Creamos la superficie original con una "gota" en el centro
-Superficie = np.zeros((N,N)).astype(np.float64)
+Superficie = zeros((N,N)).astype(float64)
 celda_centro = int(N/2)
-Superficie[celda_centro, celda_centro] = 0.5
+Superficie[celda_centro, celda_centro] = 1.0
 
 #Fijamos el aspecto de la ventana para siempre visualizar
 #celdas cuadradas
-ventana = pg.GraphicsLayoutWidget(size=(600,600))
-ventana.setWindowTitle("Simulación de difusión limitada, FPS=0.0, Num. Partículas=0")
+ventana = pg.GraphicsLayoutWidget(size=(700,700))
+ventana.setWindowTitle(f"Simulación de difusión limitada, {Nceldas=}")
 vista = ventana.addViewBox()
 vista.setAspectLocked(True)
 
@@ -39,48 +28,16 @@ vista.addItem(ImagenSuperficie)
 vista.setRange(QtCore.QRectF(0,0, N, N))
 
 #Fijando mapa de color
-cm = pg.colormap.get(name="gnuplot2", source="matplotlib")
-bar = pg.ColorBarItem(values=(0.0, 1.0), colorMap=cm)
+cm = pg.colormap.get(name="nipy_spectral", source="matplotlib")
+bar =pg.ColorBarItem(values=(0.0, 1.0), limits=(0.0, 1.0), colorMap=cm)
+ventana.addItem(bar)
 bar.setImageItem(ImagenSuperficie)
 
-#Fijamos el estado inicial
+#Generamos el patrón de difusión entorno a un elemento en el centro
+DifusionLim(Superficie, Nceldas)
 ImagenSuperficie.setImage(Superficie)
+
 ventana.show()
-
-#Iniciando la exportación de la simulación a imagenes
-particula = 0
-#exporter = ImageExporter(ventana.scene())
-#exporter.export(f".\ImagenesDifLim\DifLimitada_{particula}.png")
-
-TiempoActualizar = perf_counter()
-transcurrido = 0.0
-
-timer = QtCore.QTimer()
-timer.setSingleShot(True)
-
-print(f"Iniciando en una malla de {N}x{N} con {Nceldas} particulas...")
-
-def ActualizarSuperficie():
-    global radioMax, TiempoActualizar, transcurrido,particula
-    radioMax = DifusionLim(Superficie, kernel, PosiblesMovimientos, Nceldas, particula, radioMax)
-    ImagenSuperficie.setImage(Superficie)
-    
-    particula += 1
-
-    timer.start(1)
-    TiempoAct = perf_counter()
-    TiempoTrans = TiempoAct - TiempoActualizar
-    TiempoActualizar = TiempoAct
-    transcurrido = transcurrido*0.9 + TiempoTrans*0.1
-    ventana.setWindowTitle(f"Simulación de difusión limitada, FPS={1.0/transcurrido:.2f}, Num. Partículas={particula}")
-    #exporter.export(f".\ImagenesDifLim\DifLimitada_{particula}.png")
-
-    if particula==Nceldas:
-        timer.stop()
-
-timer.timeout.connect(ActualizarSuperficie)
-ActualizarSuperficie()
 
 if __name__=="__main__":
     pg.exec()
-    #system("ffmpeg -framerate 60 -i .\ImagenesDifLim\DifLimitada_%d.png DifLimitada.mp4")
--- RutinasDifusion.pyx
+++ RutinasDifusion.pyx
@@ -2,42 +2,103 @@
 
 import cython
 
-from random import choice
 import numpy as np
-from scipy.ndimage import convolve
 cimport numpy as np
+
+ctypedef np.float64_t Dtype_t
+
+from libc.stdlib cimport rand
+from libc.stdio cimport printf
+
+cdef extern from "math.h":
+    Dtype_t atan(Dtype_t arg) nogil
+    Dtype_t fabs(Dtype_t arg) nogil
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def DifusionLim(double[:,:] Superficie,double[:,:] kernel,list Movimientos,int Nceldas,int i,int rmax):
+@cython.nonecheck(False)
+cdef Dtype_t sumaVecinos(np.ndarray[Dtype_t, ndim=2] Arreglo, int x, int y):
     cdef:
-        int N = Superficie.shape[0]
-        int posX, posY
-        int centro = int(N/2)
+        Dtype_t suma_total = 0
+        int N = <int> Arreglo.shape[0]
+        int x1, x2, y1, y2
 
-    while True:  
-        posX, posY = np.random.randint(centro-rmax, centro+rmax, 2)
-        if Superficie[posX, posY]==0:
-            break
+    x1 = x + 1
+    x2 = x - 1
+    if x1 == N:
+        x1 = 0
+    if x2 == -1:
+        x2 = N-1
 
-    vecinos_contados = convolve(Superficie, kernel, mode="constant")
-    if vecinos_contados[posX, posY]>0:
-        Superficie[posX, posY]= 0.5 + (i+1)/(2.0*(Nceldas))
+    y1 = y + 1
+    y2 = y - 1
+    if y1 == N:
+        y1 = 0
+    if y2 == -1:
+        y2 = N-1
 
-    else:
-        while True:
-            pasox, pasoy = choice(Movimientos)
-            posX += pasox
-            posY += pasoy
-            posX, posY= posX % N, posY % N
+    suma_total = Arreglo[x,y1] + Arreglo[x,y2] + Arreglo[x1,y] + Arreglo[x2,y]
+    suma_total += Arreglo[x1,y1] + Arreglo[x1,y2] + Arreglo[x1,y1] + Arreglo[x2,y2]
 
-            if vecinos_contados[posX, posY]>0:
-                rmax += 2
-                if rmax > centro:
-                    rmax += -2
+    return suma_total
 
-                Superficie[posX, posY]= 1.0 - (i+1)/(2.0*(Nceldas))
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+def DifusionLim(np.ndarray[Dtype_t, ndim=2] Superficie,int Nceldas):
+    cdef:
+        int N = <int>Superficie.shape[0]
+        int posX, posY, dx, dy
+        int i
+        Dtype_t vecinos_contados, pi, distNorm, maxDist
+        int cenX, cenY
+    
+    pi = 4.0*atan(1.0)
+    maxDist = (2**0.5)*N
+    cenX = N/2
+    cenY = cenX
+
+    for i in range(1, Nceldas+1):
+        while True:  
+            posX = rand() % N
+            posY = rand() % N
+            if Superficie[posX, posY]==0:
                 break
-   
-    return rmax
+
+        vecinos_contados = sumaVecinos(Superficie, posX, posY)
+        if vecinos_contados>0:
+            difX = posX - cenX
+            difY = posY - cenY
+            distNorm = <Dtype_t>(difX*difX + difY*difY)**0.5 / maxDist
+            Superficie[posX, posY]= 1.0 - 4.0*fabs(atan(distNorm))/pi
+
+        else:
+            while True:
+                dx = rand() % 3 - 1
+                dy = rand() % 3 - 1
+
+                posX += dx
+                posY += dy
+
+                if posX == N:
+                    posX = 0
+                elif posX == -1:
+                    posX = N-1
+
+                if posY == N:
+                    posY = 0
+                if posY == -1:
+                    posY = N-1
+
+
+                vecinos_contados = sumaVecinos(Superficie, posX, posY)
+
+                if vecinos_contados>0:
+                    difX = posX - cenX
+                    difY = posY - cenY
+                    distNorm = <Dtype_t>(difX*difX + difY*difY)**0.5 / maxDist
+                    Superficie[posX, posY]= 1.0 - 4.0*fabs(atan(distNorm))/pi
+                    printf("Celda %d terminada con valor de %f\r", i, Superficie[posX,posY])
+                    break
